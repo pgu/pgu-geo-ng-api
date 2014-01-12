bed561bbc2f5185c24b26d7ecda94510781f89e0
Guilcher Pascal <pgu.dev@gmail.com>
2014-01-12 19:30:48 +0100

    test cors

--- api/hello.go
+++ api/hello.go
@@ -10,6 +10,8 @@ func init() {
 }
 
 func handleTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://pgu-geo-ng.appspot.com")
+	w.Header().Set("Content-Type", "application/json")
 	tasks := []string{"task 1", "task 2", "task 3", "task 4"}
 	json.NewEncoder(w).Encode(tasks)
 }
